f4ed7bfbf245f2d999f241e30e18d65ff0dcb596
nekobank <103983489+nekobank226@users.noreply.github.com>
2022-05-20 11:26:32 +0900

    完成

---
+++ import pigpio.py
@@ -0,0 +1,38 @@
+import pigpio
+import time
+import Adafruit_PCA9685
+
+#設定周波数
+SET_FREQ = 50
+
+#ピンの設定
+pi = pigpio.pi()
+pi.set_mode(14,pigpio.OUTPUT)
+pi.set_mode(15,pigpio.OUTPUT)
+
+#PCA9685の初期化
+PCA9685 = Adafruit_PCA9685.PCA9685()
+PCA9685.set_pwm_freq(SET_FREQ)
+
+#時計回りに回転
+pi.write(14,1)
+pi.write(15,0)
+PCA9685.set_pwm(0,0,1000)
+time.sleep(2)
+
+pi.write(14,1)
+pi.write(15,0)
+PCA9685.set_pwm(0,0,2000)
+time.sleep(2)
+
+#逆回転
+pi.write(14,0)
+pi.write(15,1)
+PCA9685.set_pwm(0,0,1000)
+time.sleep(2)
+
+pi.write(14,0)
+pi.write(15,1)
+PCA9685.set_pwm(0,0,2000)
+time.sleep(2)
+
