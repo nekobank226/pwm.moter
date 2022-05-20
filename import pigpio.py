a6f86228a8c36d91f162be6b7c933a232b9c825c
nekobank <103983489+nekobank226@users.noreply.github.com>
2022-05-20 11:29:03 +0900

    完成

--- import pigpio.py
+++ import pigpio.py
@@ -19,7 +19,7 @@ pi.write(14,1)
 pi.write(15,0)
 PCA9685.set_pwm(0,0,1000)
 time.sleep(2)
-
+#回転速度を早く
 pi.write(14,1)
 pi.write(15,0)
 PCA9685.set_pwm(0,0,2000)
@@ -30,7 +30,7 @@ pi.write(14,0)
 pi.write(15,1)
 PCA9685.set_pwm(0,0,1000)
 time.sleep(2)
-
+#回転速度を早く
 pi.write(14,0)
 pi.write(15,1)
 PCA9685.set_pwm(0,0,2000)
